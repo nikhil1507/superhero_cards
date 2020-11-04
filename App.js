17fb30b1edfc0975dc7c697008d1fe6d95b63392
Nikhil Thakur <nikhilthakur@sample.com>
2020-11-04 18:06:33 +0530

    Updated the files

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Details from "./screens/Details";
+import Main from "./screens/Main";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+const StackScreen = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="main" component={Main} />
+    <Stack.Screen name="details" component={Details} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Details />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
